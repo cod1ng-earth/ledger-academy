22ec343c4744b9995240a5a5316d9bb5724ea6c1
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-05-19 23:45:04 +0200

    checks current ETH Balance on load adds all test networks to oz config contains a deployed instance definition of an ADI token
    ADI token has a greeting now
    
    rebased / readme
    
    drops npm lock
    
    ignore all secrets

--- contracts/ADIToken.sol
+++ contracts/ADIToken.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts-ethereum-package/contracts/presets/ERC20PresetMinterPauser.sol";
 
 
-contract ADIToken is ERC20PresetMinterPauserUpgradeSafe {}
+contract ADIToken is ERC20PresetMinterPauserUpgradeSafe {
+    function greet() public pure returns (string memory) {
+        string memory g = "Bom Dia";
+        return g;
+    }
+}
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useWeb3React } from "@web3-react/core";
 import Web3 from "web3";
 
 const Main: React.FC = () => {
-  const context = useWeb3React<Web3>()
-  const { account } = context;
+  const { account, library: web3 } = useWeb3React<Web3>();
+
+  const [ethBalance, setEthBalance] = useState<string>("");
+
+  useEffect(() => {
+    (async () => {
+      const ethBalance = await web3.eth.getBalance(account);
+      setEthBalance(ethBalance);
+    })();
+  }, [web3]);
 
   return (
     <div>
       <p>oh hai {account}</p>
+      <p>You've got {ethBalance}ETH</p>
+      <p>to spare with others.</p>
     </div>
   );
 };
