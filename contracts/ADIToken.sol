38ef095cec89368178f2257ceac2fd682b47fcfa
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-05-19 23:01:21 +0200

    adds oz adds ADIToken without impl

---
+++ contracts/ADIToken.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/presets/ERC20PresetMinterPauser.sol";
+
+
+contract ADIToken is ERC20PresetMinterPauserUpgradeSafe {}
