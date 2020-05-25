6ae7b0efef8216f19de2134d3f572e5685ee41a4
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-05-25 11:02:55 +0200

    simple IPFS uploads

--- src/IpfsPage.tsx
+++ src/IpfsPage.tsx
@@ -1,23 +1,66 @@
 import React, { useState, useEffect } from "react";
 import { useWeb3Context } from "web3-react";
 import ADIToken from "./contracts/ADIToken.json";
-import TransferForm from "./TransferForm";
+import IPFS, { Buffer, CID } from "ipfs";
 import _secrets from "../.secrets.json";
 
 import Web3 from "web3";
 
 const IpfsPage: React.FC = () => {
   const { account, library: web3 } = useWeb3Context();
+  const [ipfsNode, setIpfsNode] = useState();
+
+  const [files, setFiles] = useState([]);
+
+  async function addToIpfs(content: string | any[]): Promise<any[]> {
+    const addResult = ipfsNode!.add(content);
+    const results = [];
+
+    for await (const result of addResult) {
+      results.push(result);
+    }
+    return results;
+  }
+
+  const submit = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const _currentContent = e.target["thecontent"].value;
+    const ipfsResult = await addToIpfs(_currentContent);
+    console.log(ipfsResult);
+
+    setFiles([...files, ipfsResult[0]]);
+  };
 
   useEffect(() => {
-    (async () => {})();
-  }, [web3]);
+    (async () => {
+      const _ipfsNode = await IPFS.create();
+      console.log("ipfs node is running");
+      setIpfsNode(_ipfsNode);
+    })();
+  }, []);
 
   return (
     <div>
-      <p>
-        oh hai <b>{account}</b>
-      </p>
+      <form onSubmit={submit}>
+        <textarea name="thecontent">test</textarea>
+        <button type="submit" value="store!">
+          store!
+        </button>
+      </form>
+      <h2>Files:</h2>
+      <ul>
+        {files.map((f) => (
+          <li>
+            <a
+              href={`https://ipfs.io/ipfs/${f.cid.toString()}`}
+              target="_blank"
+            >
+              {f.cid.toString()}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
