da2739880854ef20cefed40cfbca31716b900f6b
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-05-25 22:43:30 +0200

    demonstrate what you can not do

--- contracts/DocExchange.sol
+++ contracts/DocExchange.sol
@@ -14,4 +14,25 @@ contract DocExchange {
     function getMyDocument() public view returns (bytes memory cid) {
         return documents[msg.sender];
     }
+
+    // this would be nice. But you cannot do that. docs are storage but return types must be memory
+    // https://ethereum.stackexchange.com/questions/11926/how-to-return-a-mapping-type
+
+    /*function getAllDocuments()
+        public
+        view
+        returns (mapping(address => bytes) memory docs)
+    {
+        return documents;
+    }
+    */
+
+    //function getAllPeopleThatHaveADocument() public view returns (address[] people) {
+    // return documents.keys??
+    //}
+
+    // you only can do (depends on your mapping's value type)
+    function hasADocument(address owner) public view returns (bool) {
+        return documents[owner].length > 0;
+    }
 }
