cff04c0ddb9fdf3945c1e47fde28b60d97af5cb8
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-11-06 00:07:13 +0100

    adds an exchange (eth <-> ADI) function

--- contracts/contracts/ADIToken.sol
+++ contracts/contracts/ADIToken.sol
@@ -4,9 +4,30 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts-ethereum-package/contracts/presets/ERC20PresetMinterPauser.sol";
 
 contract ADIToken is ERC20PresetMinterPauserUpgradeSafe {
+    uint256 public ethBalance = 0;
+
     function greet() public pure returns (string memory) {
         string memory g = "Bom Dia";
         return g;
+    }
+
+    /*
+     * converts "ETH" to "ADI"
+     */
+    function exchange() public payable {
+        require(msg.value > 1e9, "send at least 1gwei (1000000000) eth");
+        require(balanceOf(address(this)) > msg.value);
+        ethBalance += msg.value;
+        _transfer(address(this), msg.sender, msg.value);
+    }
+
+    function withdraw() public {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            "only owner can withdraw"
+        );
+        msg.sender.transfer(ethBalance);
+        ethBalance = 0;
     }
 
     function airdrop(address[] memory recipients, uint256[] memory amounts)
