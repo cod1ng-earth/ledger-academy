8407bd2bfc0d27a5fe9d80ba6620ccd69b9e69f6
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-11-07 00:01:34 +0100

    adds tests for exchange and airdrop docker-compose: use .env MNEMONC sets truffle default gas to a low value

--- contracts/test/aditoken.js
+++ contracts/test/aditoken.js
@@ -22,9 +22,39 @@ contract('AdiToken', (accounts) => {
   });
 
   it('can exchange eth value', async () => {
+    const initialBalanceOfContract = await web3.eth.getBalance(adiToken.address);
+    assert.equal(0, initialBalanceOfContract);
+
     await adiToken.mint(adiToken.address, 1e10);
     await adiToken.exchange({ value: 1 + 1e9, from: accounts[2] });
-    const balance = await adiToken.balanceOf.call(accounts[2]);
-    assert.equal(balance.valueOf(), 1 + 1e9);
+
+    const tokenBalance = await adiToken.balanceOf.call(accounts[2]);
+    assert.equal(tokenBalance.valueOf(), 1 + 1e9);
+    const newBalanceOfContract = await web3.eth.getBalance(adiToken.address);
+    assert.equal(1 + 1e9, newBalanceOfContract);
+
+    const initialBalanceOfOwner = new web3.utils.BN(await web3.eth.getBalance(accounts[0]));
+    await adiToken.withdraw();
+    const newBalanceOfOwner = new web3.utils.BN(await web3.eth.getBalance(accounts[0]));
+
+    // the withdraw call costs gas, hence we cannot compare the real values here
+    // might be flaky when your gas price is larger than ~10000wei
+    assert.isTrue((new web3.utils.BN(newBalanceOfOwner)).gt(new web3.utils.BN(initialBalanceOfOwner)));
+  });
+
+  it('can airdrop a token to several recipients', async () => {
+    assert.equal(0, await adiToken.balanceOf(accounts[5]).valueOf());
+    assert.equal(0, await adiToken.balanceOf(accounts[6]).valueOf());
+
+    await adiToken.mint(accounts[4], 3000);
+
+    await adiToken.airdrop(
+      [accounts[5], accounts[6]], [2000, 1000], {
+        from: accounts[4],
+      },
+    );
+
+    assert.equal(2000, await adiToken.balanceOf(accounts[5]).valueOf());
+    assert.equal(1000, await adiToken.balanceOf(accounts[6]).valueOf());
   });
 });
