bd57bbed9452212cfbec51ffbf9f43133289ad0f
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-11-18 12:13:12 +0100

    adds default arweave server

--- src/components/pages/StoragePage.tsx
+++ src/components/pages/StoragePage.tsx
@@ -15,7 +15,13 @@ const StoragePage = () => {
   const [arweaveWallet, setArweaveWallet] = useState<ArweaveWallet>();
 
   useEffect(() => {
-    setArweave(Arweave.init({}));
+    setArweave(Arweave.init({
+      host: 'arweave.net', // Hostname or IP address for a Arweave host
+      port: 443, // Port
+      protocol: 'https', // Network protocol http or https
+      timeout: 20000, // Network request timeouts in milliseconds
+      logging: false, // Enable network request logging
+    }));
   }, []);
 
   return (<Tabs isFitted size="md" variant="enclosed-colored" variantColor="green">
